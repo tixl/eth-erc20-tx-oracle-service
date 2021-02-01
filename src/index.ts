9b381a608cbe3d79c5d65218b59fe6b704e98bc1
Bernd Strehl <bernd.strehl@gmail.com>
2021-02-01 19:45:23 +0100

    add caching

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 require('dotenv').config();
 import express from 'express';
-import { FullServiceHandlers } from './types';
+import NodeCache from 'node-cache';
+import { Mutex } from 'async-mutex';
+import { FullServiceHandlers, TransactionInformation, AssetTransactionData, CreateTransactionResponse } from './types';
 import { configureLogger, logger } from './log';
 import { handlers as ethHandler } from './implementations/eth';
 import { handlers as erc20Handler } from './implementations/erc20';
-
-// import NodeCache from 'node-cache';
-// import { Mutex } from 'async-mutex';
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'local_with_logger') {
   configureLogger(
@@ -34,19 +33,50 @@ function getHandlerForSymbol(symbol: string): FullServiceHandlers | null {
   else return null;
 }
 
+const txInfoMx = new Mutex();
+const txInfoCache = new NodeCache({ stdTTL: 10, checkperiod: 1 });
+
+async function getTransactionInfoResult(
+  handler: FullServiceHandlers,
+  symbol: string,
+  reference: string,
+  poolAddress: string,
+) {
+  const release = await txInfoMx.acquire();
+  try {
+    const key = `${symbol}-${reference}-${poolAddress}`;
+    const fromCache: TransactionInformation | undefined = txInfoCache.get(key);
+    if (fromCache) {
+      logger.info('Tx info delivering from cache', { key });
+      return fromCache;
+    } else {
+      logger.info('Tx info no cache hit', { key });
+      const result = await handler.oracle.getTransactionInformation(reference as string, poolAddress as string, symbol);
+      txInfoCache.set(key, result);
+      return result;
+    }
+  } catch (error) {
+    logger.error('Error getTransactionInfoResult', { error });
+    return null;
+  } finally {
+    release();
+  }
+}
+
 app.get('/:symbol/oracle/transactionInfo', async (req, res) => {
   const handler = getHandlerForSymbol(req.params.symbol);
   if (handler === null) return res.status(400).json({ status: 'UNSUPPORTED_SYMBOL' });
   const { reference, poolAddress } = req.query;
   logger.info('Called /oracle/transactionInfo', { reference, poolAddress });
   if (!reference || !poolAddress) return res.status(400).json({ status: 'MISSING_PARAMS' });
-  const result = await handler.oracle.getTransactionInformation(
+  const result = await getTransactionInfoResult(
+    handler,
+    req.params.symbol!,
     reference as string,
     poolAddress as string,
-    req.params.symbol,
   );
-  if (result.status === 'ERROR') {
-    return res.status(500).json(result);
+  if (result === null) {
+    res.status(500);
   }
   return res.json(result);
 });
@@ -63,6 +93,37 @@ app.post('/:symbol/oracle/validateSignature', async (req, res) => {
   return res.json(result);
 });
 
+const createTxMx = new Mutex();
+const createTxCache = new NodeCache({ stdTTL: 10, checkperiod: 1 });
+
+async function createTransaction(
+  handler: FullServiceHandlers,
+  transactionData: AssetTransactionData[],
+  symbol?: string,
+): Promise<CreateTransactionResponse | null> {
+  const release = await createTxMx.acquire();
+  try {
+    const key = JSON.stringify(transactionData);
+    const fromCache: CreateTransactionResponse | undefined = createTxCache.get(key);
+    if (fromCache) {
+      logger.info('Create tx delivering from cache', { key });
+      return fromCache;
+    } else {
+      logger.info('Create tx no cache hit', { key });
+      const result = await handler.transactionService.createTransaction(
+        transactionData as AssetTransactionData[],
+        symbol,
+      );
+      createTxCache.set(key, result);
+      return result;
+    }
+  } catch (error) {
+    return null;
+  } finally {
+    release();
+  }
+}
+
 app.post('/:symbol/tx/create', async (req, res) => {
   const handler = getHandlerForSymbol(req.params.symbol);
   if (handler === null) return res.status(400).json({ status: 'UNSUPPORTED_SYMBOL' });
@@ -71,14 +132,22 @@ app.post('/:symbol/tx/create', async (req, res) => {
   if (!transactionData) {
     return res.status(400).json({ status: 'MISSING_BODY' });
   }
-  const result = await handler.transactionService.createTransaction(transactionData, req.params.symbol);
-  if (result.status === 'ERROR') {
-    return res.status(500).json(result);
+  const result = await createTransaction(handler, transactionData as AssetTransactionData[], req.params.symbol);
+  if (result === null) {
+    return res.status(500).json({ status: 'ERROR' });
   }
-  if (result.status !== 'OK') {
-    return res.status(400).json(result);
+  switch (result.status) {
+    case 'OK':
+      return res.json(result);
+    case 'INSUFFICIENT_FUNDS':
+      return res.status(400).json({ status: result.status });
+    case 'INVALID_RECEIVER_ADDRESS':
+      return res.status(400).json({ status: result.status });
+    case 'INVALID_SENDER_ADDRESS':
+      return res.status(400).json({ status: result.status });
+    default:
+      return res.status(500).json({ status: result.status });
   }
-  return res.json(result);
 });
 
 app.post('/:symbol/tx/signAndSend', async (req, res) => {
