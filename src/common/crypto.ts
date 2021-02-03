a6e4134181587486ac284f11123a25e269c7b722
Bernd Strehl <bernd.strehl@gmail.com>
2021-02-03 16:31:37 +0100

    use correct method to recover signer in verify signature

--- src/common/crypto.ts
+++ src/common/crypto.ts
@@ -1,11 +1,18 @@
-import { utils } from 'ethers';
+import { recoverAddress } from '@ethersproject/transactions';
+import { logger } from '../log';
 
 export async function verifySignature(
   message: string,
   address: string | string[],
   signature: string,
 ): Promise<boolean> {
-  const signer = utils.verifyMessage(message, signature);
-  if (Array.isArray(address)) return address.map((x) => x.toLowerCase()).includes(signer.toLowerCase());
-  else return signer.toLowerCase() === address.toLowerCase();
+  try {
+    const signer = recoverAddress(message, signature);
+    if (Array.isArray(address)) return address.map((x) => x.toLowerCase()).includes(signer.toLowerCase());
+    else return signer.toLowerCase() === address.toLowerCase();
+  } catch (error) {
+    logger.warn('Error in verifySignature', { error });
+    console.log(error);
+    return false;
+  }
 }
