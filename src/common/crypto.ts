55300383fa7d178ecb281028190f2ad0be377e54
Bernd Strehl <bernd.strehl@gmail.com>
2021-02-05 17:18:43 +0100

    use same message hashing as wallet

--- src/common/crypto.ts
+++ src/common/crypto.ts
@@ -1,5 +1,6 @@
 import { recoverAddress } from '@ethersproject/transactions';
 import { logger } from '../log';
+import keccak256 from 'keccak256';
 
 export async function verifySignature(
   message: string,
@@ -7,7 +8,7 @@ export async function verifySignature(
   signature: string,
 ): Promise<boolean> {
   try {
-    const signer = recoverAddress(message, signature);
+    const signer = recoverAddress(keccak256('\x19Ethereum Signed Message:\n' + message.length + message), signature);
     if (Array.isArray(address)) return address.map((x) => x.toLowerCase()).includes(signer.toLowerCase());
     else return signer.toLowerCase() === address.toLowerCase();
   } catch (error) {
